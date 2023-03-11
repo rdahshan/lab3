8eb44a2de5d0c9ff86ab1a3a6b5aa7c70952ea70
rdahshan <ranaadahshan@gmail.com>
2023-03-11 20:30:20 +0200

    git

--- src/main/java/com/example/demo2/Controllers/UserController.java
+++ src/main/java/com/example/demo2/Controllers/UserController.java
@@ -1,11 +1,17 @@
 package com.example.demo2.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/signup")
     public String SignupForm(){
         return "signup.html";
+    }
+    @PostMapping("/user")
+    Public String homePage(@ModelAttribute User user){
+        return "redirect:/home.html";
     }
 }
